80b361ec0efd859ec991abcb8129824a7f0d92e4
miquel <mpuig22@gmail.com>
2017-11-19 12:43:24 +0100

    creating ROS package

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/miquel/rob/ros_tutorials/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/miquel/rob/ros_tutorials/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/miquel/rob/ros_tutorials/catkin_ws/devel;/opt/ros/kinetic" != "" else []
